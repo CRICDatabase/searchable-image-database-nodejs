9d95f9b999ffdb112f2a18d4b32077eb9ede2715
Raniere Silva <raniere@rgaiacs.com>
2020-03-21 16:20:23 -0300

    Update src/config/database.js based on docker-compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,10 @@
 'use strict'
 
-//Configura conexao local (manter uma das duas configuracoes sempre comentada)
 module.exports = {
-    host: 'localhost',
+    host: 'db',
     username: 'root',
-    password: '',
-    database: 'cric_database_2019',
+    password: '123.456',
+    database: 'cric',
     dialect: 'mysql',
     timezone: '-03:00',
     define: {
@@ -21,25 +20,3 @@ module.exports = {
     },
     logging: false
 }
-
-//configuracao conexao producao (manter uma das duas configuracoes sempre comentada)
-/*module.exports = {
-    host: 'mysql669.umbler.com',
-    username: 'cric_2019',
-    password: 'c3bad2fd135f4314bbcf722e7f437c39',
-    database: 'cric_database_19',
-    dialect: 'mysql',
-    timezone: '-03:00',
-    define: {
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-        underscored: true, //padrao das tabelas formato snake_case        
-        timestamps: true, //created_at e updated_at (automatico)
-    },
-    pool: {
-        max: 50,
-        idle: 30000,
-        acquire: 60000,
-    },
-    logging: false
-}*/
