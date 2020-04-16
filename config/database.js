ab630e6ce55379875459b1c7996d3629f2bf86de
Raniere Silva <raniere@rgaiacs.com>
2020-04-16 20:39:48 -0300

    Add config/database.js used by Sequelize CLI

---
+++ config/database.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+    development: {
+        host: "db",
+        username: "root",
+        password: "123.456",
+        database: "cric",
+        dialect: "mysql",
+        timezone: "-03:00",
+        define: {
+            charset: "utf8",
+            collate: "utf8_general_ci",
+            underscored: true,
+            timestamps: true
+        },
+        pool: {
+            max: 5,
+            idle: 30000,
+            acquire: 60000
+        },
+        logging: false
+  },
+    production: {
+        host: "db",
+        username: process.env.CRIC_DB_USERNAME,
+        password: process.env.CRIC_DB_PASSWORD,
+        database: "cric",
+        dialect: "mysql",
+        timezone: "-03:00",
+        define: {
+            charset: "utf8",
+            collate: "utf8_general_ci",
+            underscored: true,
+            timestamps: true
+        },
+        pool: {
+            max: 5,
+            idle: 30000,
+            acquire: 60000
+        },
+        logging: false
+  }
+}
