d4a4ef70b5586537caf8d1dd4a10c251fed8c303
Raniere Silva <raniere@rgaiacs.com>
2020-04-29 10:07:46 -0300

    Remove token_curinga

--- src/utils/gerador_identificador_unico.js
+++ src/utils/gerador_identificador_unico.js
@@ -1,13 +1,6 @@
 "use strict";
 
-const token_curinga = "bac8db9147ac80b4ba8a05bb0de7c4fd";
-
 module.exports = {
-
-    obterTokenCuringa() {
-        return token_curinga;
-    },
-
     gerarUuidv4() {
         return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
