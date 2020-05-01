713599eec2afb348c51121003e842a3dff7b435c
Raniere Silva <raniere@rgaiacs.com>
2020-05-01 19:56:53 -0300

    Add DOI to seeders

--- src/seeders/20200325210000-image.js
+++ src/seeders/20200325210000-image.js
@@ -6,6 +6,7 @@ module.exports = {
             {
                 id: 1,
                 nome: "example0001.jpg",
+                doi: "cric/database.1.v1",
                 codigo_lamina: "0001",
                 excluida: 0,
                 classificacao_aprovada: 1,
@@ -22,6 +23,7 @@ module.exports = {
             {
                 id: 2,
                 nome: "example0002.jpg",
+                doi: "cric/database.2.v1",
                 codigo_lamina: "0002",
                 excluida: 0,
                 classificacao_aprovada: 1,
@@ -38,6 +40,7 @@ module.exports = {
             {
                 id: 3,
                 nome: "example0003.jpg",
+                doi: "cric/database.3.v1",
                 codigo_lamina: "0003",
                 excluida: 0,
                 classificacao_aprovada: 1,
