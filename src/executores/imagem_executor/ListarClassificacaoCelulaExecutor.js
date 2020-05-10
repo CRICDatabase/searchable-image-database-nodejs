4605dbbc0e3c66c811995005d1e98449e2118c60
Raniere Silva <raniere@rgaiacs.com>
2020-05-10 20:33:19 -0300

    Fix REST API when listing classifications

--- src/executores/imagem_executor/ListarClassificacaoCelulaExecutor.js
+++ src/executores/imagem_executor/ListarClassificacaoCelulaExecutor.js
@@ -1,8 +1,10 @@
 "use strict";
 
+const HttpStatus = require("http-status-codes");
+const debug = require("debug")("database.cric:list-classification");
+
 const Excecao = require("../../utils/enumeracoes/mensagem_excecoes");
 const ObjetoExcecao = require("../../utils/enumeracoes/controle_de_excecoes");
-const HttpStatus = require("http-status-codes");
 const ValidarTipo = require("../../utils/validacao_de_tipos");
 const ImagemRepositorio = require("../../repositorios/imagem_repositorio");
 const UsuarioRepositorio = require("../../repositorios/usuario_repositorio");
@@ -16,30 +18,9 @@ module.exports = {
         const id_usuario = parseInt(req.params.id_usuario);
         const id_imagem = parseInt(req.params.id_imagem);
 
-        const analistaCompleto = await UsuarioRepositorio.obterAnalistaPorId(id_usuario);
-        const analista = {
-            id: analistaCompleto.dataValues.id,
-            total_classificacoes: analistaCompleto.dataValues.total_classificacoes,
-            total_segmentacoes: analistaCompleto.dataValues.total_segmentacoes
-        };
-
         const todasClassificacoes = await ImagemRepositorio.listarClassificacoesCelula(id_imagem, id_usuario);
-        if (todasClassificacoes.length == 0) {            
-            let resultado = {
-                id_imagem: req.params.id_imagem,
-                analista: analista,
-                celulas: todasClassificacoes
-            };
-            return resultado;
-        }
-
-        let resultado = {
-            id_imagem: req.params.id_imagem,
-            analista: analista,
-            celulas: await prepararMensagemRetorno(req, todasClassificacoes)
-        };
-
-        return resultado;
+        await prepararMensagemRetorno(todasClassificacoes);
+        return todasClassificacoes
     }
 };
 
@@ -76,34 +57,21 @@ async function validarRequisicao(req) {
     }
 }
 
-async function prepararMensagemRetorno(req, todasCelulas) {
+async function prepararMensagemRetorno(todasCelulas) {
 
-    let celulas = [];
+    let all_injuries = {};
+    await ImagemRepositorio.listarLesoes()
+    .then(
+        (response) => {
+            for (let injury of response) {
+                all_injuries[injury.dataValues.id] = injury.dataValues;
+            }
+        }
+    )
 
-    for (let i = 0; i < todasCelulas.length; i++) {
-
-        let lesao = await obterLesao(todasCelulas[i].id_lesao);
-        celulas.push({
-            id_celula: todasCelulas[i].id_celula,
-            id_classificacao: todasCelulas[i].id_classificacao,
-            tipo_analise_realizada: todasCelulas[i].tipo_analise_realizada,
-            coord_centro_nucleo_x: todasCelulas[i].coord_centro_nucleo_x,
-            coord_centro_nucleo_y: todasCelulas[i].coord_centro_nucleo_y,
-            lesao: lesao
-        });
+    for (let cell of todasCelulas) {
+        cell.lesao = all_injuries[cell.id_lesao];
     }
 
-    return celulas;
-}
-
-async function obterLesao(id_lesao) {
-
-    const lesaoCompleta = await ImagemRepositorio.obterLesaoPorId(id_lesao);
-    const lesao = {
-        id: lesaoCompleta.dataValues.id,
-        nome: lesaoCompleta.dataValues.nome,
-        detalhes: lesaoCompleta.dataValues.detalhes
-    };
-
-    return lesao;
+    return cells;
 }
