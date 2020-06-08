da759cb422b9dbd98baaf6db3471b35750c04f05
Raniere Silva <raniere@rgaiacs.com>
2020-06-08 16:01:36 -0300

    Add MySQL to Travis CI

---
+++ config/travis.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+    database: {
+        host: "127.0.0.1",
+        username: "root",
+        password: "",
+        database: "jest",
+        dialect: "mysql",
+        timezone: "-03:00",
+        define: {
+            charset: "utf8",
+            collate: "utf8_general_ci",
+            underscored: true,
+            timestamps: true
+        },
+        pool: {
+            max: 5,
+            idle: 30000,
+            acquire: 60000
+        },
+        logging: false
+    },
+};
