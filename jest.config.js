64869ab4f04d09ac0cd7b226469089b88213984f
Raniere Silva <raniere@rgaiacs.com>
2020-06-08 15:53:04 -0300

    Avoid jest setup and teardown script to run as test

--- jest.config.js
+++ jest.config.js
@@ -146,7 +146,9 @@ module.exports = {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/config/",
-    "/node_modules/"
+    "/node_modules/",
+    "setup",
+    "teardown",
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
