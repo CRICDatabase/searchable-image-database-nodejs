9bc5fcae57c7108b228b1e9f4c566ca0f5d4b035
Raniere Silva <raniere@rgaiacs.com>
2020-06-09 14:06:45 -0300

    Add REST API test example for user

---
+++ src/__tests__/user.js
@@ -0,0 +1,76 @@
+const request = require("supertest");
+const app = require("../app");
+
+let admin_token;
+
+beforeAll(() => {
+    return request(app)
+        .post(
+            "/api/v1/login"
+        )
+        .send(
+            {
+                "email": "admin@test.database.cric.com.br",
+                "senha": "123.456"
+            }
+        )
+        .then(
+            (response) => {
+                admin_token = response.body.token_autenticacao;
+            }
+        );
+});
+
+describe(
+    "annonymous /api/v1/usuarios",
+    () => {
+        test(
+            "GET /api/v1/usuarios",
+            () => {
+                return request(app)
+                    .get("/api/v1/usuarios")
+                    .then(
+                        response => {
+                            expect(response.statusCode).toBe(401);
+                        }
+                    );
+            }
+        );
+    }
+);
+
+describe(
+    "admin /api/v1/usuarios",
+    () => {
+        test(
+            "GET /api/v1/usuarios",
+            () => {
+                return request(app)
+                    .get("/api/v1/usuarios")
+                    .set(
+                        'token_autenticacao',
+                        admin_token
+                    )
+                    .then(
+                        response => {
+                            expect(response.statusCode).toBe(200);
+                            expect(
+                                response.body
+                            ).toMatchObject(
+                                expect.arrayContaining(
+                                    [{
+                                        "ativo": 1,
+                                        "email": expect.any(String),
+                                        "id": expect.any(Number),
+                                        "primeiro_nome": expect.any(String),
+                                        "senha": expect.any(String),
+                                        "ultimo_nome": expect.any(String),
+                                    }]
+                                )
+                            );
+                        }
+                    );
+            }
+        );
+    }
+);
