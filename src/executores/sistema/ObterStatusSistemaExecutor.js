5a3df92fb8309e25e3a84de072cbb959595d9e94
Raniere Silva <raniere@rgaiacs.com>
2020-06-19 13:33:39 -0300

    Test rotasIndex.js
    Close https://github.com/CRICDatabase/searchable-image-database-nodejs/issues/106

--- src/executores/sistema/ObterStatusSistemaExecutor.js
+++ src/executores/sistema/ObterStatusSistemaExecutor.js
@@ -5,12 +5,9 @@ const {version} = require("../../../package.json");
 module.exports = {
 
     Executar() {
-
-        const informacoesDoSistema = {
+        return {
             descricao: "Center for Recognition and Inspection of Cells (CRIC) Database",
             versao: version
         };
-
-        return informacoesDoSistema;
     }
 };
