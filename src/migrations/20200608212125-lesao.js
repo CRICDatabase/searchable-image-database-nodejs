da851193e85ac8b9f4e1200a6177eec77de481d4
costascaio <caio.soares@aluno.ufop.edu.br>
2020-07-01 12:48:09 -0300

    assing descricao, lesao and image to cervix collection

--- src/migrations/20200608212125-lesao.js
+++ src/migrations/20200608212125-lesao.js
@@ -2,12 +2,25 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.addColumn('lesao', 'id_collection', Sequelize.BIGINT, {
-            allowNull: false,
-            references: { model: "collection", key: "id" },
-            onUpdate: "CASCADE",
-            onDelete: "RESTRICT"
-         });
+        return queryInterface.addColumn('lesao', 'id_collection', Sequelize.BIGINT, 
+            {
+                allowNull: false,
+                references: { model: "collection", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "RESTRICT"
+            }
+        )
+        .then(
+            ()=>{
+                return queryInterface.bulkUpdate('lesao', 
+                {
+                    id_collection: 1,
+                },{
+                    nome: '*',
+                }
+                );
+            }
+        );
     },
 
     down: (queryInterface) => {
--- src/migrations/20200608213507-descricao.js
+++ src/migrations/20200608213507-descricao.js
@@ -2,12 +2,25 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.addColumn('descricao', 'id_collection', Sequelize.BIGINT, {
-            allowNull: false,
-            references: { model: "collection", key: "id" },
-            onUpdate: "CASCADE",
-            onDelete: "RESTRICT"
-         });
+        return queryInterface.addColumn('descricao', 'id_collection', Sequelize.BIGINT, 
+            {
+                allowNull: false,
+                references: { model: "collection", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "RESTRICT"
+            }
+        )
+        .then(
+            ()=>{
+                return queryInterface.bulkUpdate('descricao', 
+                {
+                    id_collection: 1,
+                },{
+                    codigo: '*',
+                }
+                );
+            }
+        );
     },
 
     down: (queryInterface) => {
