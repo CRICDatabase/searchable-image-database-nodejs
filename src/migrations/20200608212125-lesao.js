6ff110d9fcdeaa540caa1a12ae2853d2e5206817
costascaio <caio.soares@aluno.ufop.edu.br>
2020-07-08 13:51:21 -0300

    update migrations descricao, lesao and image

--- src/migrations/20200608212125-lesao.js
+++ src/migrations/20200608212125-lesao.js
@@ -12,12 +12,17 @@ module.exports = {
         )
         .then(
             ()=>{
-                return queryInterface.bulkUpdate('lesao', 
-                {
-                    id_collection: 1,
-                },{
-                    nome: '*',
-                }
+                return Promise.all(
+                    [
+                        queryInterface.sequelize.query(
+                        `
+                            UPDATE
+                                lesao
+                            SET
+                                id_collection = 1
+                        `
+                        )
+                    ]
                 );
             }
         );
